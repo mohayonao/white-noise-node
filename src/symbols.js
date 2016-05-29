b04091d105e82fd638b7380cc24f4968f2cd70c4
mohayonao <mohayonao@gmail.com>
2016-05-29 14:43:13 +0900

    update dev tools

--- src/symbols.js
+++
@@ -1,3 +0,0 @@
-export const CONTEXT = typeof Symbol !== "undefined" ? Symbol("CONTEXT") : "@mohayonao/white-noise-source:CONTEXT";
-export const BUFSRC = typeof Symbol !== "undefined" ? Symbol("BUFSRC") : "@mohayonao/white-noise-source:BUFSRC";
-export const OUTLET = typeof Symbol !== "undefined" ? Symbol("OUTLET") : "@mohayonao/white-noise-source:OUTLET";
